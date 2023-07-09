9bede64e4e9fd15265748b24e22a8b64ca7b5bc3
Shamasulhaq <shamsulhaq@beyonderissolutions.com>
2023-07-09 18:08:03 +0500

    change in

--- coresite/settings/base_setting.py
+++ coresite/settings/base_setting.py
@@ -1,4 +1,5 @@
 from .environment_setting import env, os, BASE_DIR
+import django_heroku
 from .application_setting import (
     DJANGO_APPLICATIONS,
     CUSTOM_APPLICATIONS,
@@ -54,3 +55,4 @@ STATIC_URL = '/static/'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
