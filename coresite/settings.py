35e1afda8638c463b9a8ee559c56505ccbea4347
Shamasulhaq <shamsulhaq@beyonderissolutions.com>
2023-07-11 01:10:24 +0500

    cas7

--- coresite/settings.py
+++ coresite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 import environ
-# import django_heroku
+import django_heroku
 from pathlib import Path
 
 env = environ.Env(
@@ -53,8 +53,8 @@ INSTALLED_APPS = [
     'corsheaders',
     # included django community APPS:
     'rest_framework',
-    'drf_yasg',
-    'drf_yasg2',
+    # 'drf_yasg',
+    # 'drf_yasg2',
     'inventory',
     # self started apps:
     'core',
@@ -174,4 +174,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # SWAGGER_SETTINGS = {
 #     'DEFAULT_INFO': 'import.path.to.urls.api_info',
 # }
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
